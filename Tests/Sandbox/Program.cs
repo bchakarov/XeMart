14eb6a720cb248b6f26389f394c8da57c8f1f960
bchakarov <borko5283@abv.bg>
2020-11-07 18:58:09 +0200

    Remove Setting entity and it's corresponding tests and controllers

--- Tests/Sandbox/Program.cs
+++ Tests/Sandbox/Program.cs
@@ -11,7 +11,6 @@
     using XeMart.Data.Models;
     using XeMart.Data.Repositories;
     using XeMart.Data.Seeding;
-    using XeMart.Services.Data;
     using XeMart.Services.Messaging;
 
     using CommandLine;
@@ -52,9 +51,6 @@
         {
             var sw = Stopwatch.StartNew();
 
-            var settingsService = serviceProvider.GetService<ISettingsService>();
-            Console.WriteLine($"Count of settings: {settingsService.GetCount()}");
-
             Console.WriteLine(sw.Elapsed);
             return await Task.FromResult(0);
         }
@@ -81,7 +77,6 @@
 
             // Application services
             services.AddTransient<IEmailSender, NullMessageSender>();
-            services.AddTransient<ISettingsService, SettingsService>();
         }
     }
 }
