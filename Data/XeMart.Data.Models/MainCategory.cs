1863721a6bedc5dbb597c229893aca6313038e27
bchakarov <borko5283@abv.bg>
2020-11-07 23:11:38 +0200

    Add main category fontawesome icon and image url

--- Data/XeMart.Data.Models/MainCategory.cs
+++ Data/XeMart.Data.Models/MainCategory.cs
@@ -15,6 +15,10 @@
         [Required]
         public string Name { get; set; }
 
+        public string FontAwesomeIcon { get; set; }
+
+        public string ImageUrl { get; set; }
+
         public virtual ICollection<Subcategory> Subcategories { get; set; }
     }
 }
