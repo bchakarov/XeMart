79a8860933f4c2f0a9f3a923bdb14596b01f1289
bchakarov <borko5283@abv.bg>
2020-11-30 21:30:40 +0200

    Modify UserFavouriteProduct entity

---
+++ Data/XeMart.Data/Migrations/20201130181805_ModifyUserFavouriteProductEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XeMart.Data.Migrations
+{
+    public partial class ModifyUserFavouriteProductEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserFavouriteProducts_IsDeleted",
+                table: "UserFavouriteProducts");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "UserFavouriteProducts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserFavouriteProducts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "UserFavouriteProducts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserFavouriteProducts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavouriteProducts_IsDeleted",
+                table: "UserFavouriteProducts",
+                column: "IsDeleted");
+        }
+    }
+}
