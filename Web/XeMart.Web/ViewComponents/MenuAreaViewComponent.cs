a81faba7c44661f4db3f361e7929c3e86ab87b9e
bchakarov <borko5283@abv.bg>
2020-11-30 18:15:15 +0200

    Move menu area in view component

--- Web/XeMart.Web/ViewComponents/MenuAreaViewComponent.cs
+++ Web/XeMart.Web/ViewComponents/MenuAreaViewComponent.cs
@@ -5,11 +5,11 @@
     using XeMart.Services.Data;
     using XeMart.Web.ViewModels.Categories;
 
-    public class MenuAllCategoriesViewComponent : ViewComponent
+    public class MenuAreaViewComponent : ViewComponent
     {
         private readonly IMainCategoriesService mainCategoriesService;
 
-        public MenuAllCategoriesViewComponent(IMainCategoriesService mainCategoriesService)
+        public MenuAreaViewComponent(IMainCategoriesService mainCategoriesService)
         {
             this.mainCategoriesService = mainCategoriesService;
         }
