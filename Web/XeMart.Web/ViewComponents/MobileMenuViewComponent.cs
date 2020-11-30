5c355d748894dedaf84bc5ab2bd1f94a7b1d3fc3
bchakarov <borko5283@abv.bg>
2020-11-30 17:38:24 +0200

    Add mobile menu partial and view component

---
+++ Web/XeMart.Web/ViewComponents/MobileMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace XeMart.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using XeMart.Services.Data;
+    using XeMart.Web.ViewModels.Categories;
+
+    public class MobileMenuViewComponent : ViewComponent
+    {
+        private readonly IMainCategoriesService mainCategoriesService;
+
+        public MobileMenuViewComponent(IMainCategoriesService mainCategoriesService)
+        {
+            this.mainCategoriesService = mainCategoriesService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = this.mainCategoriesService.GetAll<MainCategoriesWithSubcategoriesViewModel>();
+            return this.View(categories);
+        }
+    }
+}
