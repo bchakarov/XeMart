a4e587e5609ff71e5f18114320668c5151881ee9
bchakarov <borko5283@abv.bg>
2020-12-04 01:36:37 +0200

    Add CartBodyCheckoutViewComponent

---
+++ Web/XeMart.Web/ViewComponents/CartBodyCheckoutViewComponent.cs
@@ -0,0 +1,47 @@
+﻿namespace XeMart.Web.ViewComponents
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using XeMart.Services;
+    using XeMart.Services.Data;
+    using XeMart.Web.ViewModels.ShoppingCart;
+
+    public class CartBodyCheckoutViewComponent : ViewComponent
+    {
+        private readonly IShoppingCartService shoppingCartService;
+        private readonly IStringService stringService;
+
+        public CartBodyCheckoutViewComponent(
+            IShoppingCartService shoppingCartService,
+            IStringService stringService)
+        {
+            this.shoppingCartService = shoppingCartService;
+            this.stringService = stringService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userId = this.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var products = await this.shoppingCartService.GetAllProducts<ShoppingCartProductViewModel>(this.User.Identity.IsAuthenticated, this.HttpContext.Session, userId);
+
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    product.ProductName = this.stringService.TruncateAtWord(product.ProductName, 15);
+                }
+            }
+
+            var viewModel = new ShoppingCartViewModel
+            {
+                Products = products ?? new List<ShoppingCartProductViewModel>(),
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
