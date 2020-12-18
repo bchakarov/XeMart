da6dd288de4be8af92ca57ca48544ce822a9e12c
bchakarov <borko5283@abv.bg>
2020-12-19 00:43:25 +0200

    Add StringService and TimeSpanService tests

---
+++ Tests/XeMart.Services.Tests/StringServiceTests.cs
@@ -0,0 +1,34 @@
+﻿namespace XeMart.Services.Tests
+{
+    using Xunit;
+
+    [Collection("Sequential")]
+    public class StringServiceTests
+    {
+        [Fact]
+        public void TruncateAtWordShouldReturnNullWithNullInput()
+        {
+            var service = new StringService();
+            Assert.Null(service.TruncateAtWord(null, 30));
+        }
+
+        [Fact]
+        public void TruncateAtWordShouldReturnTheSameInputWhenItsLengthIsShorterThanTheTruncateLength()
+        {
+            var service = new StringService();
+            var input = "Test Input";
+            Assert.Equal(input, service.TruncateAtWord(input, input.Length + 1));
+        }
+
+        [Theory]
+        [InlineData("Test Input", 7, "Test…")]
+        [InlineData("Test Input", 25, "Test Input")]
+        [InlineData("Test Input Test", 14, "Test Input…")]
+        [InlineData("Test ", 5, "Test…")]
+        public void TruncateAtWordShouldWorkCorrectly(string input, int length, string expected)
+        {
+            var service = new StringService();
+            Assert.Equal(expected, service.TruncateAtWord(input, length));
+        }
+    }
+}
---
+++ Tests/XeMart.Services.Tests/TimeSpanServiceTests.cs
@@ -0,0 +1,50 @@
+﻿namespace XeMart.Services.Tests
+{
+    using System;
+
+    using Xunit;
+
+    [Collection("Sequential")]
+    public class TimeSpanServiceTests
+    {
+        [Fact]
+        public void TruncateAtWordShouldWorkCorrectlyWithDays()
+        {
+            var service = new TimeSpanService();
+            Assert.Equal("1 days", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromDays(1))));
+            Assert.Equal("2 days", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromDays(2))));
+        }
+
+        [Fact]
+        public void TruncateAtWordShouldWorkCorrectlyWithHours()
+        {
+            var service = new TimeSpanService();
+            Assert.Equal("1 hours", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromHours(1))));
+            Assert.Equal("2 hours", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromHours(2))));
+        }
+
+        [Fact]
+        public void TruncateAtWordShouldWorkCorrectlyWithMinutes()
+        {
+            var service = new TimeSpanService();
+            Assert.Equal("1 minutes", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(1))));
+            Assert.Equal("2 minutes", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(2))));
+        }
+
+        [Fact]
+        public void TruncateAtWordShouldWorkCorrectlyWithSeconds()
+        {
+            var service = new TimeSpanService();
+            Assert.Equal("1 seconds", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(1))));
+            Assert.Equal("2 seconds", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(2))));
+        }
+
+        [Fact]
+        public void TruncateAtWordShouldWorkCorrectlyWithMilliseconds()
+        {
+            var service = new TimeSpanService();
+            Assert.Equal("a bit", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromMilliseconds(10))));
+            Assert.Equal("a bit", service.GetTimeSince(DateTime.UtcNow.Subtract(TimeSpan.FromMilliseconds(20))));
+        }
+    }
+}
